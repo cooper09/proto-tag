61860c6164a2d81823f07eaafde8afa1748aa59e
cooper09 <cooperatkmba@gmail.com>
2020-04-16 12:22:02 -0400

    nice working dictionary

--- proto-tag.js
+++ proto-tag.js
@@ -2,28 +2,69 @@
 // simple function to remove any 0 that comes after another 0
 //
 ( function( ) {
-
-    const data = [1,2,0,6,0,0,0,8,0,0,1,0,0,0];
     
     const Tracker = new function(){
-        console.log("The Lone Tracker is here: ", data );
-        this.items = {}
-        this.cartObj = []
+        console.log("The Lone Tracker is here: ");
+        this.datastore = []
 
-    const sortedArr = data.map( (num, index, array) => {
-        console.log("current num: ", num )
-            if( num == 0) {
-                //check to see if next num is 0
-                if (array[index+1] !== 0 ) {
-                    this.cartObj.push(num)
-                }//end if
-            } else {
-                this.cartObj.push(num)
-            }//end if  
-        })//end map
-    
-        console.log("final goodies: ", this.cartObj );
+        this.addItem = (key, value ) => {
+            console.log("Tracker.addItem key: ", key, " value: ", value )
+            if (key && value ) {
+                this.datastore.push({key: key, value: value})
+                return this.datastore
+            }//end iff
+        }//end add
 
+        this.checkIn = (key) => {
+            for (var i=0; this.datastore.length ; ++i) {
+                if ( this.datastore[i].key === key) {
+                    this.datastore[i].value.available = true;
+                    return this.datastore;
+                }//end iffy
+            }//end for 
+            return this.datastore;
+        }//end checkIn
+
+        this.checkOut = (key) => {
+            for (var i=0; this.datastore.length ; ++i) {
+                if ( this.datastore[i].key === key) {
+                    this.datastore[i].value.available = false;
+                    return this.datastore;
+                }//end iffy
+            }//end for 
+            return this.datastore;
+        }//end checkOut
+
+        this.findAt = (key) =>{
+            console.log("FindAt: ", key )
+            for (var i=0; i < this.datastore.length ; ++i) {
+                
+                if ( this.datastore[i].key === key) {
+                    return this.datastore[i].value.title;
+                }//end iffy
+             
+            }//end for 
+            return "Book not found" //this.datastore
+        } //end findAt
+
+        this.size = () => {
+            return this.datastore.length;
+        }//end size
+
+        this.displayData = () =>{
+            console.log("Current Data Store: ", this.datastore )
+        }// displayData
     }//end Tracker
+
+    Tracker.displayData();
+    Tracker.addItem("book0", {title:"Moby Dick", available: true })
+    Tracker.displayData();
+    Tracker.checkOut("book0")
+    Tracker.displayData()
+    Tracker.checkIn("book0")
+    Tracker.displayData()
+    console.log("found book: " , Tracker.findAt("book0"))
+    Tracker.addItem("book1", {title:"Man of La Mancha", available: false })
+    console.log("found book: " , Tracker.findAt("book1"))
 })()
 
